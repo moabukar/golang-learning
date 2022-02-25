e00080e7a0198b0329c4f81dc98ad3bbe551a37f
mohamed.abukar <mohamed.abukar@capgemini.com>
2022-02-25 22:27:04 +0000

    messing around with go

--- for-testing/main.go
+++ for-testing/main.go
@@ -21,9 +21,60 @@ package main
 
 import "fmt"
 
-var deckSize int
+// var deckSize int
+
+// func main() {
+// 	deckSize = 50
+// 	fmt.Println(deckSize)
+// }
+
+// func main() {
+// 	a := []int{5, 4, 3, 2, 1}
+// 	a = append(a, 213)
+
+// 	fmt.Println(a)
+// }
+
+// using maps below
+
+// func main() {
+// 	vertices := make(map[string]int)
+
+// 	vertices["triangle"] = 2
+// 	vertices["circle"] = 3
+// 	vertices["dodecagon"] = 4
+// 	fmt.Println(vertices)
+// }
+
+// for loops
+
+// func main() {
+// 	for i := 0; i < 6; i++ {
+// 		fmt.Println(i)
+// 	}
+
+// }
+
+// while loops
+
+// func main() {
+
+// 	i := 0
+
+// 	for i < 6 {
+// 		fmt.Println(i)
+// 		i++
+// 	}
+
+// }
+
+//
 
 func main() {
-	deckSize = 50
-	fmt.Println(deckSize)
+
+	arr := []string{"a", "b", "c"}
+
+	for index, value := range arr {
+		fmt.Println("index", index, "value")
+	}
 }
