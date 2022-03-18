de66bc8adbf306564cf9f7da1d62f4bdef7a0cda
mohamed.abukar <mo-abukar10@hotmail.com>
2022-03-18 21:25:32 +0000

    range + slide + problem

--- fundamentals/range_slice_array.go
+++ fundamentals/range_slice_array.go
@@ -35,10 +35,25 @@ import "fmt"
 // }
 
 func main() {
-	// make a slice
-	a := make([]int, 5)
-	fmt.Println(a)
 
-	// get type of data >> which is a slice ([]int)
-	fmt.Printf("%T", a)
+	var a []int = []int{2, 3, 4, 5, 5, 4, 7}
+
+	// for i := 0; i < len(a); i++ {
+	// 	fmt.Println(a[i])
+	// }
+
+	// for i, element := range a {
+	// 	fmt.Printf("%d: %d\n", i, element)
+	// }
+	// for _, element := range a {
+	// 	fmt.Printf("%d\n", element)
+	// }
+	for i, element := range a {
+		for j := i + 1; j < len(a); j++ {
+			element2 := a[j]
+			if element2 == element {
+				fmt.Println(element)
+			}
+		}
+	}
 }
