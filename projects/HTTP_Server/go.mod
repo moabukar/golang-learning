939061e98aa5eb32c77a85f9411710fc93dababf
mohamed.abukar <mo-abukar10@hotmail.com>
2022-03-20 21:19:50 +0000

    create real http server with shopping list

---
+++ projects/HTTP_Server/go.mod
@@ -0,0 +1,3 @@
+module github.com/moabukar/golang-learning/tree/main/projects/HTTP_Server
+
+go 1.17
---
+++ projects/HTTP_Server/main.go
@@ -0,0 +1,23 @@
+// package main
+
+// import "net/http"
+
+// func main() {
+// 	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
+// 		w.Write([]byte("hello world !!!!"))
+// 	})
+// 	http.ListenAndServe(":80", nil)
+// }
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/mabukar/golang-learning/tree/main/projects/HTTP_Server/api"
+)
+
+func main() {
+	srv := api.NewServer()
+	http.ListenAndServe(":8080", srv)
+}
